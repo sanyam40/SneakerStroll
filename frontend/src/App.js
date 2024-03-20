160bab2a3eea3517de08220bc89308794a45531e
sanyam <sanyamnarang40@gmail.com>
2024-03-21 00:44:55 +0530

    updted

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import { Routes, Route} from 'react-router-dom';
 import HomePage from './screens/HomePage';
-import SignUpForm from './Components/SignUpForm';
-import LoginPage from './screens/LoginPage';
 
 function App() {
   return (
     <>
     <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/SignUp' element={<LoginPage/>}/>
+        {/* <Route path='/SignUp' element={<LoginPage/>}/> */}
     </Routes>
     </>
   );
